902097573b03ec0f756931ac32547a08ab02716a
Hayrullah Sarac <hayrullahsarac48@hotmail.com>
2022-08-05 20:25:12 +0300

    first

---
+++ src/main/java/atmpackage/Runner.java
@@ -0,0 +1,13 @@
+package atmpackage;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        Account account = new Account();
+        account.getCheckingDeposit();
+
+        Options options = new Options();
+        options.login();
+    }
+}
